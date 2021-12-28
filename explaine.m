5fd248f2e3104e583773f311331cd93df91e1d80
insivium <96776048+insivium@users.noreply.github.com>
2021-12-28 18:03:13 +0400

    Create explaine.m

---
+++ explaine.m
@@ -0,0 +1,13 @@
+% calling this function will explain briefly Euler's number.
+function explaine
+  fprintf('The constant e is called the natural')
+  fprintf(' exponential base.\n')
+  fprintf('It is used extensively in math and')
+  fprintf(' engineering.\n')
+  fprintf('The value of the constant e is ~ 2.7183\n')
+  fprintf('Raising e to the power of x is so common that')
+  fprintf(' this is called the exponential function.\n')
+  fprintf('An approximation for e is found using the limit.\n')
+  fprintf('An approximation for the exponential function')
+  fprintf(' can be found using the Maclaurin series.\n')
+end
